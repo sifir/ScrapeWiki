6a493c4cc9aa86593b6b75b6ef7f74974899c501
dseptem <d.septem@gmail.com>
2016-07-27 19:52:20 -0300

    fixes varios
    seteo del resultado de la busqueda al TextView en lugar del EditText,
    scrape completo del articulo con subtitulos (incluyendo summary al
    principio), puntos y aparte

--- app/src/main/java/ar/com/sifir/scrapewiki/Scraper.java
+++ app/src/main/java/ar/com/sifir/scrapewiki/Scraper.java
@@ -1,6 +1,7 @@
 package ar.com.sifir.scrapewiki;
 
 import android.os.AsyncTask;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import org.jsoup.Jsoup;
@@ -13,40 +14,43 @@ import java.io.IOException;
  * Created by Sifir on 21/07/2016.
  */
 public class Scraper extends AsyncTask<Void, Void, Void> {
-    String parrafo;
-    String busqueda;
-    TextView tv;
+    String info; // lo que va a setearse en el TextView
+    String busqueda; // el texto que viene en el EditText y que se agrega a la url de wikipedia para buscar el articulo
+    TextView tv; // en donde va a ponerse la String info al final de todo el proceso
+    EditText et; // solo sirve para sacar la String busqueda
 
-    Scraper(TextView tv) {
+    Scraper(EditText et, TextView tv) {
         this.tv = tv;
-        busqueda = tv.getText().toString();
+        this.et = et;
+        busqueda = this.et.getText().toString();
     }
     @Override
     protected Void doInBackground(Void... params) {
         Document doc = null;
 
         try {
-            doc = Jsoup.connect("https://en.wikipedia.org/wiki/"+busqueda).get();
+            doc = Jsoup.connect("https://en.wikipedia.org/wiki/"+busqueda).get(); // parsea el articulo de wikipedia como un documento de jsoup
         } catch (IOException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
+        //empieza a armar la string info que va a ponerse en el TextView tv
+        info = "Summary\n\n"; //los articulos no tienen un subtitulo sumario al principio, asi que lo agregué, el \n\n es como apretar dos enter
+        info += doc.select(".mw-headline, p").get(0).text(); //1er parrafo
 
-        parrafo = doc.select(".mw-headline, p").get(0).text();
-        Element parraf = doc.select(".mw-headline, p").get(0);
-
-        while (parraf.nextElementSibling().hasText()) {
-            parraf = parraf.nextElementSibling();
-            if (parraf.tagName().contains("headline")) {
-                parrafo += "\n\n"+"\n\n";
+        for (Element e : doc.select(".mw-headline, p")) { //por cada elemento ("casi seria un parrafo"), hace...
+            if (e.tagName().contains("mw-headline")) { //si es el tagName del elemento contiene "headline" en su nombre, agrega un espacio extra antes de escribir el subtitulo
+                info += "\n";
             }
-            parrafo += parraf.text() + "\n";
+            info += "\n"; //si no es "headline", solo tiene un punto y aparte
+            info += e.text();  //agrega el texto del elemento al string q estamos construyendo
         }
 
         return null;
     }
 
     protected void onPostExecute(Void result) {
-        tv.setText(parrafo);
+        tv.setSingleLine(false); // setea al TextView para multilinea
+        tv.setText(info);        // setea la string armada
     }
 }
